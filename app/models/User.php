f4f458e9f8da8db37a89e09479c11e24bc9ea365
Retchel Tapayan <rtapayan@global-fusion.net>
2014-11-28 17:52:35 +0800

    Facebook login test

--- app/models/User.php
+++ app/models/User.php
@@ -6,8 +6,34 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
 
-	 protected $table = 'user';
+/**
+ * Get the unique identifier for the user.
+ *
+ * @return mixed
+ */
+public function getAuthIdentifier()
+{
+    return $this->getKey();
+}
 
+/**
+ * Get the password for the user.
+ *
+ * @return string
+ */
+public function getAuthPassword()
+{
+    return $this->password;
+}
+
+/**
+ * Get the e-mail address where password reminders are sent.
+ *
+ * @return string
+ */
+public function getReminderEmail()
+{
+    return $this->email;
 }
 
 ?>
