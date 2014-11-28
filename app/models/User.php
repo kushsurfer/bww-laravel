aacc528210a8a4ade793f8ee3cbf52a39106cce8
Retchel Tapayan <rtapayan@global-fusion.net>
2014-11-28 17:50:35 +0800

    Facebook login test

--- app/models/User.php
+++ app/models/User.php
@@ -1,6 +1,9 @@
 <?php 
 
-class User extends Eloquent {
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class User extends Eloquent implements UserInterface, RemindableInterface {
 
 
 	 protected $table = 'user';
