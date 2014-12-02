247ec97041d5ba207ec079885854c2eacb8af38f
Retchel Tapayan <rtapayan@global-fusion.net>
2014-12-02 20:17:44 +0800

    test amazon login

--- app/views/shop_view/shop.blade.php
+++ app/views/shop_view/shop.blade.php
@@ -211,11 +211,11 @@
 	</div>
 
 
-<a href="#" id="LoginWithAmazon">
+<!-- <a href="#" id="LoginWithAmazon">
   <img border="0" alt="Login with Amazon"
     src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
     width="156" height="32" />
-</a>
+</a> -->
 <script type="text/javascript">
 
   document.getElementById('LoginWithAmazon').onclick = function() {
